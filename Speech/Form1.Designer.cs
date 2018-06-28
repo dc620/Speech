5e3c1af9427ff4f3f1764b1a3cf03472fe8e7ec5
Divakar <divakar6220@live.com>
2018-06-28 19:27:45 +0530

    Bing Speech API

---
+++ Speech/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Speech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbLogs = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbLogs
+            // 
+            this.tbLogs.Location = new System.Drawing.Point(6, 2);
+            this.tbLogs.Multiline = true;
+            this.tbLogs.Name = "tbLogs";
+            this.tbLogs.Size = new System.Drawing.Size(782, 456);
+            this.tbLogs.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(608, 464);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "button1";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(713, 465);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 2;
+            this.btnEnd.Text = "button2";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 492);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tbLogs);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbLogs;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEnd;
+    }
+}
+
---
+++ Speech/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using Microsoft.CognitiveServices.SpeechRecognition;
+namespace Speech
+{
+    public partial class Form1 : Form
+    {
+        private MicrophoneRecognitionClient micClient;
+
+        private string SubscriptionKey = "f05d9b035d884225b67023c63fa4e0ee";
+        private SpeechRecognitionMode Mode = SpeechRecognitionMode.LongDictation;
+        private string DefaultLocale = "en-US";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (micClient == null)
+                CreateMicrophoneRecoClient();
+
+            micClient.StartMicAndRecognition();
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            micClient.EndMicAndRecognition();
+        }
+        private void CreateMicrophoneRecoClient()
+        {
+            micClient = SpeechRecognitionServiceFactory.CreateMicrophoneClient(
+                Mode,
+                DefaultLocale,
+                SubscriptionKey);
+            micClient.AuthenticationUri = "";
+
+            // Event handlers for speech recognition results
+            micClient.OnMicrophoneStatus += OnMicrophoneStatus;
+            //  micClient.OnPartialResponseReceived += OnPartialResponseReceivedHandler;
+            micClient.OnResponseReceived += OnMicDictationResponseReceivedHandler;
+            micClient.OnConversationError += OnConversationErrorHandler;
+        }
+
+        private void OnMicrophoneStatus(object sender, MicrophoneEventArgs e)
+        {
+            //Dispatcher dispatcherUI = Dispatcher.CurrentDispatcher;
+            tbLogs .BeginInvoke (new Action(() =>
+            {
+                if (e.Recording)
+                {
+                    WriteLine("Please start speaking ... :)");
+                }
+
+                WriteLine("");
+            }));
+        }
+
+        private void OnPartialResponseReceivedHandler(object sender, PartialSpeechResponseEventArgs e)
+        {
+            WriteLine("{0}", e.PartialResult);
+            WriteLine("");
+        }
+
+        private void OnMicDictationResponseReceivedHandler(object sender, SpeechResponseEventArgs e)
+        {
+            if (e.PhraseResponse.RecognitionStatus == RecognitionStatus.EndOfDictation ||
+                e.PhraseResponse.RecognitionStatus == RecognitionStatus.DictationEndSilenceTimeout)
+            {
+                // Dispatcher dispatcherUI = Dispatcher.CurrentDispatcher;
+                tbLogs.BeginInvoke(new Action(() =>
+                {
+                        micClient.EndMicAndRecognition();
+                    }));
+            }
+
+            if (e.PhraseResponse.Results.Length > 0)
+            {
+                WriteLine(e.PhraseResponse.Results.OrderByDescending(x => x.Confidence).First().DisplayText);
+            }
+        }
+
+        private void OnConversationErrorHandler(object sender, SpeechErrorEventArgs e)
+        {
+            WriteLine("Error: {0}", e.SpeechErrorText);
+            WriteLine("");
+        }
+
+        private void WriteLine(string format, params object[] args)
+        {
+            var formattedStr = string.Format(format, args);
+            Trace.WriteLine(formattedStr);
+            //    Dispatcher dispatcherUI = Dispatcher.CurrentDispatcher;
+            tbLogs.BeginInvoke(new Action(() =>
+            {
+                tbLogs.Text += formattedStr + "\n";
+               // tbLogs.ScrollToEnd();
+            }));
+        }
+    }
+}
